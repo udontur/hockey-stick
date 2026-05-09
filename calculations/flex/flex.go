4a437b5d6f27aef979587a4a56b7c09176282482
Hadrian <h@hadrian.cc>
2026-05-09 18:47:16 +0800

    feat: flex calculator

---
+++ calculations/flex/flex.go
@@ -0,0 +1,13 @@
+package flex
+
+import (
+	"math"
+)
+
+func calculate(weightKg int) int {
+	const flexFactor float64=1.1023
+
+	var exactFlex float64=float64(weightKg)*flexFactor
+	var roundedFlex int=int(math.Round(exactFlex))
+	return roundedFlex
+}
