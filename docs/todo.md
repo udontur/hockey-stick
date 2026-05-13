3c77123fecb50a96494787ab8054a8d45479f9b2
Hadrian <h@hadrian.cc>
2026-05-13 21:42:34 +0800

    feat: simple bubble tea TUI

--- docs/todo.md
+++ docs/todo.md
@@ -2,5 +2,13 @@
 - [ ] Check for GO's best practice
 - [ ] Format and check for bad code
 - [x] Installation (GoReleaser: .deb, .rpm, .tar.xz binary)
-- [ ] use Charm VHS to record a demo 
+- [x] use Charm VHS to record a demo 
 - [x] Make Readme with demo, install instructions
+
+_____________________
+| Your hockey stick |
+_____________________
+|Curve   |   P92    |
+|Flex    |    52    |
+|Length  |   58"    | 
+_____________________
--- hockey/display/display.go
+++ hockey/display/display.go
@@ -2,13 +2,87 @@ package display
 
 import (
 	"fmt"
+	"os"
 	"hockey-stick/hockey/types"
+	"strconv"
+	tea "charm.land/bubbletea/v2"
 )
 
+type info struct {
+	name string
+	value string
+}
+
+type model struct {
+	stickInfo []info
+	cursor int
+}
+
+func initialModel(stick types.Stick) model {
+	var arr = make([]info, 5)
+	arr[0].name="Hand"
+	arr[0].value=stick.Hand
+	arr[1].name="Curve"
+	arr[1].value=stick.Curve
+	arr[2].name="Flex"
+	arr[2].value=strconv.Itoa(stick.Flex)
+	arr[3].name="Kick"
+	arr[3].value=stick.Kick
+	arr[4].name="Length"
+	arr[4].value=strconv.Itoa(stick.Length)
+	return model{
+		stickInfo: arr,
+	}
+}
+
+func (m model) Init() tea.Cmd {
+	return nil
+}
+
+func (m model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
+	switch msg:=msg.(type){
+		case tea.KeyPressMsg:
+			switch msg.String() {
+				case "ctrl+c", "q":
+					return m, tea.Quit
+				case "up":
+					if m.cursor > 0 {
+						m.cursor--
+					}
+				case "down":
+					if m.cursor < len(m.stickInfo)-1{
+						m.cursor++
+					}
+			}
+	}
+	return m, nil
+}
+
+func (m model) View() tea.View {
+	var header="Your ideal hockey stick\n\n"
+	for i, spec := range m.stickInfo {
+		cursor:=" "
+		if m.cursor==i{
+			cursor=">"
+		}
+
+		header+=fmt.Sprintf("%s #%v %v: %s\n", cursor, i, spec.name, spec.value)
+	}
+	header+="\nPress q to quite\n"
+	return tea.NewView(header)
+}
+
 func DisplayResult(stick types.Stick) {
-	fmt.Println(stick.Curve)
-	fmt.Println(stick.Flex)
-	fmt.Println(stick.Hand)
-	fmt.Println(stick.Kick)
-	fmt.Println(stick.Length)
+	var p=tea.NewProgram(initialModel(stick))
+	if _, err:=p.Run(); err!=nil{
+		fmt.Printf("Oh hell naw error: %v", err)
+		os.Exit(1)
+	}
+	// fmt.Println(stick.Curve)
+	// fmt.Println(stick.Flex)
+	// fmt.Println(stick.Hand)
+	// fmt.Println(stick.Kick)
+	// fmt.Println(stick.Length)
+
+
 }
